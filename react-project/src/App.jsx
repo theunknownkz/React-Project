b83c8cc26f59ca232f710ba121e392aaeee16593
theunknownkz <theunknownkz@gmail.com>
2025-08-28 21:57:18 +0800

    Added a dynamic list

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,6 +1,6 @@
-import './App.css'
+import "./App.css"
 
-function Header({name, year}) {
+function Header({ name, year }) {
   return (
     <header>
       <h1>Hi {name}</h1>
@@ -9,13 +9,34 @@ function Header({name, year}) {
   )
 }
 
+const items = [
+  "One",
+  "Two",
+  "Three"
+]
+
+const numberObjects = items.map((number, i) => ({
+  id: i,
+  title: number
+}))
+
+function Main({ numbers }) {
+  return (
+    <>
+      <ul>
+        {numbers.map((number) =>
+        <li key={number.id} style={{ listStyleType: "None" }}>{number.title}
+        </li>)}
+      </ul>
+    </>
+  )
+}
+
 function App() {
   return (
   <div>
     <Header name="Alex" year={new Date().getFullYear()} />
-    <main>
-      <h2>Yes</h2>
-    </main>
+    <Main numbers={numberObjects} />
   </div>
   )
 }
