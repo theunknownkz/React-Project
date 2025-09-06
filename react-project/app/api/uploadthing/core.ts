9f65486ecdef92173b944414f63ddb88ed981148
theunknownkz <theunknownkz@gmail.com>
2025-09-06 22:51:30 +0800

    Image upload bug fixing in progress

--- react-project/app/api/uploadthing/core.ts
+++ react-project/app/api/uploadthing/core.ts
@@ -17,14 +17,25 @@ export const ourFileRouter = {
       maxFileCount: 1,
     },
   })
+    // Set permissions and file types for this FileRoute
+    .middleware(async ({ req }) => {
+      // This code runs on your server before upload
+      const session = await auth();
+
+      // If you throw, the user will not be able to upload
+      if (!session || !session.user) throw new UploadThingError("Unauthorized");
+
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: session.user.id };
+    })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
-      console.log("Upload complete for userId:", "metadata.userId");
+      console.log("Upload complete for userId:", metadata.userId);
 
       console.log("file url", file.ufsUrl);
 
       // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-      return { uploadedBy: "metadata.userId" };
+      return { uploadedBy: metadata.userId };
     }),
 } satisfies FileRouter;
 
--- react-project/app/trips/new/page.tsx
+++ react-project/app/trips/new/page.tsx
@@ -4,10 +4,13 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { createTrip } from "@/lib/actions/create-trip";
 import { cn } from "@/lib/utils";
-import { useTransition } from "react";
+import { UploadButton } from "@/lib/upload-thing";
+import { useState, useTransition } from "react";
+import Image from "next/image";
 
 export default function NewTrip() {
     const [isPending, startTransition] = useTransition()
+    const [imageUrl, setImageUrl] = useState<string | null>(null)
     return (
         <div className="max-w-lg mx-auto mt-10">
             <Card>
@@ -66,6 +69,30 @@ export default function NewTrip() {
                                                 "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500")}
                                 />
                             </div>
+                        </div>
+
+                        <div>
+                            <label>Trip Image</label>
+                            {imageUrl && (
+                                <Image
+                                  src={imageUrl}
+                                  alt="Trip Preview"
+                                  className="w-full mb-4 rounded-md max-h-48 object-cover"
+                                  width={300}
+                                  height={100}
+                                />
+                            )}
+                            <UploadButton
+                                endpoint="imageUploader"
+                                onClientUploadComplete={(res) => {
+                                    if (res && res[0].ufsUrl) {
+                                        setImageUrl(res[0].ufsUrl)
+                                    }
+                                }}
+                                onUploadError={(error: Error) => {
+                                    console.error("Upload error: ", error);
+                                }}
+                            />
                         </div>
 
                         <Button type="submit" disabled={isPending} className="w-full">
