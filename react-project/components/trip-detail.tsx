1a589e03fa013e1ead1714103d32903e6245cb4b
theunknownkz <theunknownkz@gmail.com>
2025-09-10 23:40:34 +0800

    Added a button that allows users to add a location for the trip

--- react-project/components/trip-detail.tsx
+++ react-project/components/trip-detail.tsx
@@ -2,6 +2,9 @@
 
 import { Trip } from "@/app/generated/prisma";
 import Image from "next/image";
+import { Calendar, Plus } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
 
 interface TripDetailClientProps {
   trip: Trip;
@@ -23,7 +26,25 @@ export default function TripDetailClient({ trip }: TripDetailClientProps) {
       )}
 
       <div className="bg-white p-6 shadow rounded-lg flex flex-col md:flex-row justify-between items-start md:items-center">
-        <div></div>
+        <div>
+          <h1 className="text-4xl font-extrabold text-gray-900">
+            {trip.title}
+          </h1>
+          <div className="flex items-center text-gray-500 mt-2">
+            <Calendar className="h-5 w-5 mr-2" />
+            <span className="text-lg">
+              {trip.startDate.toLocaleDateString()} -{" "}
+              {trip.endDate.toLocaleDateString()}
+            </span>
+          </div>
+        </div>
+        <div className="mt-4 md:mt-0">
+          <Link href={`/trips/${trip.id}/itinerary/new`}>
+            <Button>
+              <Plus /> Add Location
+            </Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
