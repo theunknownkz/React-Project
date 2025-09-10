fec6da18fddd2253b62602111c2fb64741d08235
theunknownkz <theunknownkz@gmail.com>
2025-09-10 23:11:13 +0800

    Trip image can now be displayed on the page

--- react-project/components/trip-detail.tsx
+++ react-project/components/trip-detail.tsx
@@ -1,11 +1,30 @@
 "use client";
 
 import { Trip } from "@/app/generated/prisma";
+import Image from "next/image";
 
 interface TripDetailClientProps {
   trip: Trip;
 }
 
 export default function TripDetailClient({ trip }: TripDetailClientProps) {
-  return <></>;
+  return (
+    <div className="container mx-auto px-4 py-8 space-y-8">
+      {trip.imageUrl && (
+        <div className="w-full h-72 md:h-96 overflow-hidden rounded-xl shadow-lg relative">
+          <Image
+            src={trip.imageUrl}
+            alt={trip.title}
+            className="object-cover"
+            fill
+            priority
+          />
+        </div>
+      )}
+
+      <div className="bg-white p-6 shadow rounded-lg flex flex-col md:flex-row justify-between items-start md:items-center">
+        <div></div>
+      </div>
+    </div>
+  );
 }
